b536fcafdaf6568f0fcc971a5bf2f581a7e4c56a
Дмитрий <135257648+dimais77@users.noreply.github.com>
2024-03-24 10:11:43 +0700

    Add new files

--- main.py
+++ main.py
@@ -8,7 +8,10 @@ eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 while True:
     success, frame = video.read()
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    eyes = eye_cascade.detectMultiScale(gray_frame, scaleFactor=1.3, minNeighbors=2, minSize=(10, 10))
+    eyes = eye_cascade.detectMultiScale(gray_frame, scaleFactor=1.3, minNeighbors=2, minSize=(30, 30))
+
+    if len(eyes) == 0:
+        continue
 
     # Инициализируем переменные для хранения координат единой прямоугольной области
     min_x, min_y = float('inf'), float('inf')
@@ -22,24 +25,25 @@ while True:
         max_y = max(max_y, y + h)
 
     # Добавляем смещение к координатам прямоугольной области
-    offset = 30  # Выбераем значение для увеличени я области
-    min_x -= offset
-    min_y -= 0
-    max_x += offset
-    max_y += 0
+    offset_x = 30
+    offset_y = 0
+    min_x -= offset_x
+    min_y -= offset_y
+    max_x += offset_x
+    max_y += offset_y
 
     # Создаем прямоугольную область, охватывающую глаза
-    eyes_roi = frame[min_y:max_y, min_x:max_x]
-
-    # На случай, если "глаза потеряются" )))
-    if not eyes_roi.size:
-        continue
+    eyes_roi = frame[int(min_y):int(max_y), int(min_x):int(max_x)]
 
     # Размываем область глаз
-    blurred_roi = cv2.GaussianBlur(eyes_roi, (45, 15), 20)
+    try:
+        blurred_roi = cv2.GaussianBlur(eyes_roi, (45, 15), 20)
+    except cv2.error as e:
+        print("Error:", e)
+        continue
 
     # Заменяем прямоугольную область в кадре на размытую область
-    frame[min_y:max_y, min_x:max_x] = blurred_roi
+    frame[int(min_y):int(max_y), int(min_x):int(max_x)] = blurred_roi
 
     # Отображаем кадр с размытой областью глаз
     cv2.imshow('Eyes Detection', frame)
